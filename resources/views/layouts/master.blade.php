1df479266b7f12ef3f04ab95d820dca9659356ff
Ricky Noel Diancin Jr <rickyxmarter@gmail.com>
2016-12-12 14:45:49 +0800

    added layouts folder

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        </head>
+<body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Welcome to Laravel 5</div>
+            </div>
+        </div>
+    </body>
+</html>
