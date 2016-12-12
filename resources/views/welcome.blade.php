3dd91626c031977bf7b9b52d7f71e484878ea3ea
Ricky Noel Diancin Jr <rickyxmarter@gmail.com>
2016-12-13 00:24:02 +0800

    added login form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,23 @@ Welcome!
 @endsection
 
 @section('content')
-This is the content!
+ <div class="row">
+ <div class="col-6">
+     <form action="#" method="post">
+     <div class="form-group">
+     <label for="email">Your Email</label>
+     <input class="form-control" type="text" name="email" id="email">
+     </div>
+   <div class="form-group">
+     <label for="first_name">Your First Name</label>
+     <input class="form-control" type="text" name="first_name" id="first_name">
+     </div>
+        <div class="form-group">
+     <label for="password">Your Password</label>
+     <input class="form-control" type="password" name="password" id="password">
+     </div>
+<button type="submit" class="btn btn-primary">Submit</button>
+     </form>
+ </div>
+ </div>
 @endsection
