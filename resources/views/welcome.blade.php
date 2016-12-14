b5aa2f687608d2443663ba423b9bf84c388361e4
Ricky Noel Diancin Jr <rickyxmarter@gmail.com>
2016-12-14 13:30:55 +0800

    change app title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@ Welcome!
 
 @section('content')
  <div class="row">
- <div class="col-6">
-   <h3>Sign In</h3>
+ <div class="col-md-6">
+   <h3>Sign Up</h3>
      <form action="#" method="post">
      <div class="form-group">
      <label for="email">Your Email</label>
@@ -23,17 +23,14 @@ Welcome!
 <button type="submit" class="btn btn-primary">Submit</button>
      </form>
  </div>
-  <div class="col-6">
-  <h3>Sign Up</h3>
+  <div class="col-md-6">
+  <h3>Sign In</h3>
      <form action="#" method="post">
      <div class="form-group">
      <label for="email">Your Email</label>
      <input class="form-control" type="text" name="email" id="email">
      </div>
-   <div class="form-group">
-     <label for="first_name">Your First Name</label>
-     <input class="form-control" type="text" name="first_name" id="first_name">
-     </div>
+
         <div class="form-group">
      <label for="password">Your Password</label>
      <input class="form-control" type="password" name="password" id="password">
