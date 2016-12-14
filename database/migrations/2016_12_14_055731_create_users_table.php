9f8761300cc3bc5c65dca5754a457ad8dbec5df5
Ricky Noel Diancin Jr <rickyxmarter@gmail.com>
2016-12-14 14:07:00 +0800

    added user fields on User Model

--- database/migrations/2016_12_14_055731_create_users_table.php
+++ database/migrations/2016_12_14_055731_create_users_table.php
@@ -15,6 +15,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('email');
+            $table->string('password');
+            $table->rememberToken();
         });
     }
 
