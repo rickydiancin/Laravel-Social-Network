7a33140e82a213d5f43098175e5d5c057a5d5ba8
Ricky Noel Diancin Jr <rickyxmarter@gmail.com>
2016-12-23 15:56:54 +0800

    added old helper function on inputs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,15 @@ Welcome!
      <form action="{{ route('signup')}}" method="post">
      <div class="form-group">
      <label for="email">Your Email</label>
-     <input class="form-control" type="text" name="email" id="email">
+     <input class="form-control" type="text" name="email" id="email" value="{{ Request::old('email')}}">
      </div>
    <div class="form-group">
      <label for="first_name">Your First Name</label>
-     <input class="form-control" type="text" name="first_name" id="first_name">
+     <input class="form-control" type="text" name="first_name" id="first_name" value="{{ Request::old('first_name')}}">
      </div>
         <div class="form-group">
      <label for="password">Your Password</label>
-     <input class="form-control" type="password" name="password" id="password">
+     <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password')}}">
      </div>
 <button type="submit" class="btn btn-primary">Submit</button>
 <input type="hidden" name="_token" value="{{ Session::token() }}">
@@ -30,12 +30,12 @@ Welcome!
      <form action="{{ route('signin') }}" method="post">
      <div class="form-group">
      <label for="email">Your Email</label>
-     <input class="form-control" type="text" name="email" id="email">
+     <input class="form-control" type="text" name="email" id="email" value="{{ Request::old('email')}}">
      </div>
 
         <div class="form-group">
      <label for="password">Your Password</label>
-     <input class="form-control" type="password" name="password" id="password">
+     <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password')}}">
      </div>
 <button type="submit" class="btn btn-primary">Submit</button>
 <input type="hidden" name="_token" value="{{ Session::token() }}">
